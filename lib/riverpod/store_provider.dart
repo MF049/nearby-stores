8e5ea7d88dc8127d2abeb2d51214a5004c90f89c
MF049 <82678465+MF049@users.noreply.github.com>
2025-04-28 01:30:28 +0300

    Update store_provider.dart

--- lib/riverpod/store_provider.dart
+++ lib/riverpod/store_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:geolocator/geolocator.dart';
 import '../models/store.dart';
